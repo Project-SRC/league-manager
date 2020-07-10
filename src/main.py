a66413fbfd25f16e3eae1d2a76bd50038bff25a3
João Pedro Sconetto <sconetto.joao@gmail.com>
2020-07-10 14:17:22 +0200

    feat: update service
    - add new routes for base apis
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- src/main.py
+++ src/main.py
@@ -2,6 +2,16 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.openapi.utils import get_openapi
 from fastapi.openapi.docs import get_swagger_ui_html
 from environs import Env
+from src.api.team.contract import ROUTER as CONTRACT
+from src.api.country.country import ROUTER as COUNTRY
+from src.api.user.driver import ROUTER as DRIVER
+from src.api.league.league import ROUTER as LEAGUE
+from src.api.user.manager import ROUTER as MANAGER
+from src.api.race.participation import ROUTER as PARTICIPATION
+from src.api.race.race import ROUTER as RACE
+from src.api.user.steward import ROUTER as STEWARD
+from src.api.team.team import ROUTER as TEAM
+from src.api.track.track import ROUTER as TRACK
 from src.api.user.user import ROUTER as USER
 import uvicorn
 import ujson as json
@@ -11,7 +21,7 @@ env.read_env()
 
 MOCK = env.bool("MOCK", default=False)
 TEST = env.bool("TEST", default=False)
-VERSION = env.str("VERSION")
+VERSION = env.str("VERSION", default="0.0.1")
 
 league = FastAPI(
     title="League API 🏎🏆🏁",
@@ -20,7 +30,17 @@ league = FastAPI(
 )
 
 # Add Custom Models and Routes
-league.include_router(USER, prefix="/user", tags=["User"])
+league.include_router(COUNTRY, prefix="/v1/country", tags=["Country"])
+league.include_router(LEAGUE, prefix="/v1/league", tags=["League"])
+league.include_router(PARTICIPATION, prefix="/v1/race/participation", tags=["Race"])
+league.include_router(RACE, prefix="/v1/race", tags=["Race"])
+league.include_router(TEAM, prefix="/v1/team", tags=["Team"])
+league.include_router(CONTRACT, prefix="/v1/team/contract", tags=["Team"])
+league.include_router(TRACK, prefix="/v1/track", tags=["Track"])
+league.include_router(USER, prefix="/v1/user", tags=["User"])
+league.include_router(DRIVER, prefix="/v1/user/driver", tags=["User"])
+league.include_router(MANAGER, prefix="/v1/user/manager", tags=["User"])
+league.include_router(STEWARD, prefix="/v1/user/steward", tags=["User"])
 
 
 @league.get("/")
