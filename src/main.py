78c86d4831018a70b56d2f419cd54d6bedda4df1
João Pedro Sconetto <sconetto.joao@gmail.com>
2020-08-04 18:01:57 +0200

    feat(routes): update routes prefixes
    - change router for objects that have a base model
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- src/main.py
+++ src/main.py
@@ -24,28 +24,27 @@ TEST = env.bool("TEST", default=False)
 VERSION = env.str("VERSION", default="0.0.1")
 
 league = FastAPI(
-    title="League API 🏎🏆🏁",
-    description="Project SRC - API",
-    version=VERSION
+    title="League API 🏎🏆🏁", description="Project SRC - API", version=VERSION
 )
 
 # Add Custom Models and Routes
 league.include_router(COUNTRY, prefix="/v1/country", tags=["Country"])
 league.include_router(LEAGUE, prefix="/v1/league", tags=["League"])
-league.include_router(PARTICIPATION, prefix="/v1/race/participation", tags=["Race"])
+league.include_router(PARTICIPATION, prefix="/v1/race", tags=["Race"])
 league.include_router(RACE, prefix="/v1/race", tags=["Race"])
 league.include_router(TEAM, prefix="/v1/team", tags=["Team"])
-league.include_router(CONTRACT, prefix="/v1/team/contract", tags=["Team"])
+league.include_router(CONTRACT, prefix="/v1/team", tags=["Team"])
 league.include_router(TRACK, prefix="/v1/track", tags=["Track"])
 league.include_router(USER, prefix="/v1/user", tags=["User"])
-league.include_router(DRIVER, prefix="/v1/user/driver", tags=["User"])
-league.include_router(MANAGER, prefix="/v1/user/manager", tags=["User"])
-league.include_router(STEWARD, prefix="/v1/user/steward", tags=["User"])
+league.include_router(DRIVER, prefix="/v1/user", tags=["User"])
+league.include_router(MANAGER, prefix="/v1/user", tags=["User"])
+league.include_router(STEWARD, prefix="/v1/user", tags=["User"])
 
 
 @league.get("/")
 async def read_root():
     return {"Hello": "World"}
+
 
 if __name__ == "__main__":
     uvicorn.run("src.main:league", host="127.0.0.1", port=8000, reload=True)
